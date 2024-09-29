cb0ee662bce88181a7e233b9afe3013ba87c93e5
Filip Brouwers <filip.brouwers@gmail.com>
2024-09-29 22:00:54 +0200

    put bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// import climber1 from './components/img_climber1/climber1';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import CollapsibleExample from './components/NavbarClimb.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Under development
-        </p>
+    <div>
+      <CollapsibleExample />
 
-      </header>
+
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+          <div className="container">
+            <div className='row'>
+              <div className='col-3'>
+                <img className="img-fluid" src={require("/home/filgood/code/Fil-good/react/belgianclimbers/src/images/IMG_20240710_074832440.jpg")} alt={"belgianClimber"} />
+              </div>
+              <div className='col-3'>
+                <img className="img-fluid" src={require("/home/filgood/code/Fil-good/react/belgianclimbers/src/images/IMG_20240710_093805643 bucce arancia start.jpg")} alt={"belgianClimber1"} />
+              </div>
+            </div>
+          </div>
+
     </div>
+
   );
 }
 
